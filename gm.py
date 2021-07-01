d69b3c71896c4b3cd6d27b0887953de8d516b710
FrostLace <61438933+FrostLace@users.noreply.github.com>
2021-07-01 07:38:42 -0400

    init

---
+++ gm.py
@@ -0,0 +1,31 @@
+class Gm:
+    def __init__(self,ntiles,npoles=3):
+        self.ntiles = ntiles
+        self.npoles = npoles
+        self.board = [[None for y in range(self.ntiles)] for x in range(self.npoles)]
+        self.board[0] = list(range(ntiles))
+        self.board[0].reverse()
+    def firstNone(self,pole,subtract=0):
+        for i in range(self.ntiles):
+            if self.board[pole][i] is None:
+                return i-subtract
+        return i
+    def move(self,polea,poleb):
+        a = self.firstNone(polea,1)
+        b = self.firstNone(poleb)
+        print(a,b)
+        if self.board[poleb][0] is not None:
+            if self.board[polea][a] > self.board[poleb][b-1]:
+                return 1
+        del self.board[poleb][b]
+        self.board[poleb].insert(b,self.board[polea].pop(a))
+        self.board[polea].insert(a,None)
+        return 0
+
+
+
+if __name__ == "__main__":
+    a = Gm(3,3)
+    a.move(0,2)
+    a.move(0,2)
+    print(a.board)
