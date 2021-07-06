a8a021b56bb1994070e3b601e36cbc8866c76db7
FrostLace <61438933+FrostLace@users.noreply.github.com>
2021-07-05 20:41:24 -0400

    drawboard is essentially finished(minus color)

--- ui.py
+++ ui.py
@@ -13,15 +13,18 @@ from gm import Gm
 SPACECH = " "
 TILECH = "▄"
 POLECH = "█"
-'''
+
 scr = curses.initscr()
 curses.noecho()
 curses.cbreak()
 scr.keypad(True)
-'''
-def tile(num,max_num):
-    radius = num + 2
-    max_radius = max_num + 2
+
+def tileString(num,max_num):
+    if num is not None:
+        radius = num + 2
+    else:
+        radius = 0
+    max_radius = max_num + 3
     empty = max_radius - radius
     first_half = ""
     for ch in range(max_radius):
@@ -32,11 +35,20 @@ def tile(num,max_num):
     tile_string = first_half + POLECH + first_half[::-1]
     return tile_string
 
-def drawBoard(board):
-    
-'''
+def drawBoard(x,y,board,max_tile):
+    for l in range(max_tile+1):
+        line = ""
+        for p in board:
+            line += tileString(list(reversed(p))[l],max_tile)
+        scr.addstr(y+l,x,line)
+    line = ""
+    for i in board:
+        line += tileString(max_tile+1,max_tile)
+    scr.addstr(y+1+l,x,line)
+
+drawBoard(0,0,[[2,1,0],[None,None,None],[None,None,None]],2)
+
 curses.nocbreak()
 scr.keypad(False)
 curses.echo()
 curses.endwin()
-'''
